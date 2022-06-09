1358c4ccd0277260a96494b1ed06fe4ea84f2d5e
LFA <lfernandoareas@gmail.com>
2022-06-09 07:12:44 -0300

    aula 13 - implementando Repositório

--- src/main/java/guru/springframework/reactiveexamples/PersonRepositoryImpl.java
+++ src/main/java/guru/springframework/reactiveexamples/PersonRepositoryImpl.java
@@ -1,2 +1,21 @@
-package guru.springframework.reactiveexamples;public class PersonRepositoryImpl {
+package guru.springframework.reactiveexamples;
+
+import guru.springframework.reactiveexamples.domain.Person;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public class PersonRepositoryImpl implements PersonRepository {
+
+    Person michael = new Person(1, "Michael", "Jordan");
+    Person bobby  = new Person(2, "Bobby", "Fischer");
+    Person tim  = new Person(3, "Timothy", "Keller");
+    Person poirot  = new Person(4, "Hercule", "Poirot");
+    @Override
+    public Mono<Person> getById(Integer id) {
+        return Mono.just(michael);
+    }
+    @Override
+    public Flux<Person> findAll() {
+        return Flux.just(michael, bobby, tim, poirot);
+    }
 }
